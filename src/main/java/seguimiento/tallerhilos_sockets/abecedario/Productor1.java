b0e01440da5cb3926bcb32a30a4e803bca8cdd41
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-11-05 14:14:53 -0500

    Se agrega actualizacion al repositorio

--- src/main/java/seguimiento/tallerhilos_sockets/abecedario/Productor1.java
+++ src/main/java/seguimiento/tallerhilos_sockets/abecedario/Productor1.java
@@ -1,9 +1,15 @@
 package seguimiento.tallerhilos_sockets.abecedario;
 
+import java.util.ArrayList;
+
 public class Productor1 extends Thread {
 
-    private Tuberia tuberia;
+    Tuberia tuberia;
+
+
     private String alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private ArrayList<Character> listaLetras = obtenerLista();
 
     public Productor1( Tuberia t )
     {
@@ -14,11 +20,17 @@ public class Productor1 extends Thread {
     public void run() {
         char c;
 
-        // Mete 10 letras en la tubería
-        for( int i=0; i < 10; i++ )
+        // Cada productor podria meter un maximo de 13 letras a la tuberia
+        for( int i=0; i < 13; i++ )
         {
             c = alfabeto.charAt( (int)(Math.random()*26 ) );
-            tuberia.lanzar( c );
+
+            if(!buscarElemento(listaLetras, 0, listaLetras.size()-1, c)){
+                listaLetras.add(c);
+                agregarLetra(listaLetras);
+                tuberia.lanzar( c );
+            }
+
             // Imprime un registro con lo a�adido
             System.out.println( "Productor 1 Lanzó "+c+" a la tuberia." );
             // Espera un poco antes de añadir más letras
@@ -31,5 +43,26 @@ public class Productor1 extends Thread {
                 System.out.println(e);
             }
         }
+    }
+
+    private static boolean buscarElemento(ArrayList<Character> array, int inicial, int fin, Character numeroBuscado) {
+
+        if(inicial > fin){
+            return false;
+        }else {
+            if(array.get(inicial).equals(numeroBuscado)){
+                return true;
+            }else {
+                return buscarElemento(array, inicial+1, fin, numeroBuscado);
+            }
+        }
+    }
+
+    public ArrayList<Character> obtenerLista(){
+        return tuberia.getLetrasConsumidas();
+    }
+
+    public void agregarLetra(ArrayList<Character> listaLetras){
+        tuberia.setLetrasConsumidas(listaLetras);
     }
 }
--- src/main/java/seguimiento/tallerhilos_sockets/abecedario/Productor2.java
+++ src/main/java/seguimiento/tallerhilos_sockets/abecedario/Productor2.java
@@ -1,9 +1,13 @@
 package seguimiento.tallerhilos_sockets.abecedario;
 
+import java.util.ArrayList;
+
 public class Productor2 extends Thread{
 
-    private Tuberia tuberia;
+    Tuberia tuberia;
     private String alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private ArrayList<Character> listaLetras = obtenerLista();
 
     public Productor2( Tuberia t )
     {
@@ -14,13 +18,19 @@ public class Productor2 extends Thread{
     public void run() {
         char c;
 
-        // Mete 10 letras en la tubería
-        for( int i=0; i < 10; i++ )
+        // Cada productor podria meter un maximo de 13 letras a la tuberia
+        for( int i=0; i < 13; i++ )
         {
             c = alfabeto.charAt( (int)(Math.random()*26 ) );
-            tuberia.lanzar( c );
+
+            if(!buscarElemento(listaLetras, 0, listaLetras.size()-1, c)){
+                listaLetras.add(c);
+                agregarLetra(listaLetras);
+                tuberia.lanzar( c );
+            }
+
             // Imprime un registro con lo a�adido
-            System.out.println( "Productor 2 Lanzó "+c+" a la tuberia." );
+            System.out.println( "Productor 1 Lanzó "+c+" a la tuberia." );
             // Espera un poco antes de añadir más letras
             try
             {
@@ -31,6 +41,27 @@ public class Productor2 extends Thread{
                 System.out.println(e);
             }
         }
+    }
+
+    private static boolean buscarElemento(ArrayList<Character> array, int inicial, int fin, Character numeroBuscado) {
+
+        if(inicial > fin){
+            return false;
+        }else {
+            if(array.get(inicial).equals(numeroBuscado)){
+                return true;
+            }else {
+                return buscarElemento(array, inicial+1, fin, numeroBuscado);
+            }
+        }
+    }
+
+    public ArrayList<Character> obtenerLista(){
+        return tuberia.getLetrasConsumidas();
+    }
+
+    public void agregarLetra(ArrayList<Character> listaLetras){
+        tuberia.setLetrasConsumidas(listaLetras);
     }
 
 }
