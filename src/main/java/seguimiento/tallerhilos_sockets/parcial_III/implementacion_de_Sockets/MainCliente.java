9ec98260eddbe82ac09521d274d1ba797ab45864
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-11-15 23:01:21 -0500

    Se agrega el paqueta parcial III para desarrollar algunos ejemplos

---
+++ src/main/java/seguimiento/tallerhilos_sockets/parcial_III/implementacion_de_Sockets/MainCliente.java
@@ -0,0 +1,4 @@
+package seguimiento.tallerhilos_sockets.parcial_III.implementacion_de_Sockets;
+
+public class MainCliente {
+}
---
+++ src/main/java/seguimiento/tallerhilos_sockets/parcial_III/implementacion_de_Sockets/TipoVehiculo.java
@@ -0,0 +1,8 @@
+package seguimiento.tallerhilos_sockets.parcial_III.implementacion_de_Sockets;
+
+public enum TipoVehiculo {
+
+    automóvil,
+    motocicleta,
+    camioneta
+}
